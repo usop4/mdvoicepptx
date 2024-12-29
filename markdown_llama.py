e2a040f55c119da713ce15c87eb89adecbc5ee55
Takayuki Uehara <t.uehara@gmail.com>
2024-12-29 21:27:11 +0900

    markdown_llamaがようやく動作（ただし品質が良くない）

--- markdown_llama.py
+++ markdown_llama.py
@@ -9,12 +9,14 @@ from langchain_core.messages import AIMessage
 
 def make_comment(s):
     messages = [
-        ("system","この文に含まれる韓国語の文法をコンパクトに日本語で解説してください",),
+        ("system","この文に含まれる韓国語の文を１〜５単語程度で区切って、それぞれをコンパクトに日本語で解説してください。文は数字やカッコは付けないでください。発音や日本語の読みについては解説しないでください。中国語、漢字語は書かないでください。",),
         ("human", s),
     ]
     ai_msg = llm.invoke(messages)
-
     return ai_msg.content
+
+def make_comment_test(s):
+    return ">>"+s
 
 def is_korean(text):
     for char in text:
@@ -28,11 +30,18 @@ if __name__ == "__main__":
     temp = ""
     fname = "scenario.md"
     with open(fname, 'r', encoding='utf-8') as file:
-        for i, line in enumerate(file, start=1):
-            line = line.strip()
-            #ic(line)
-            if is_korean(line):
-                res = make_comment(line)
-                print(line)
-                print(res.strip())
+        lines = file.readlines()
+    
+    i = 0
+    while i < len(lines):
+        if is_korean(lines[i]):
+            res = make_comment(lines[i])
+            lines.insert(i + 1, " " + res.strip() + "\n")
+            i += 1  # コメント行をスキップするためにインクリメント
+        i += 1
+
+    # 最後にfnameに書き込みする
+    with open(fname, 'w', encoding='utf-8') as file:
+        file.writelines(lines)
+
 
